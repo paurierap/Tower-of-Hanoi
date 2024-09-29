9857b12186d94611a242d2e3b811d380ed15097d
Pau Riera <paurierap@github.com>
2024-09-29 19:50:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TOWER OF HANOI SOLVER
+
+Minor project to solve the classic puzzle [Tower of Hanoi](https://en.wikipedia.org/wiki/Tower_of_Hanoi), based on 3 different wooden pegs where the main goal is to move a stack of $n$ disks from an initial peg (peg A) to a target one (C), while using a spare peg (peg B). The rules to do so are the following:
+
+1. Only one disk may be moved at a time.
+2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty peg.
+3. No disk may be placed on top of a disk that is smaller than it.
+
+The minimal number of moves required to solve a Tower of Hanoi puzzle is $2^n − 1$.
+
+![The Tower of Hanoi game](tower-hanoi.jpg)
---
+++ main.cpp
@@ -0,0 +1,112 @@
+/****************************************************************************
+ * TOWER OF HANOI PUZZLE SOLVER ******************************************** 
+
+    Based on the famous puzzle Tower of Hanoi (https://en.wikipedia.org/wiki/Tower_of_Hanoi):
+        - A player has 3 pegs A, B and C.
+        - Peg A initially has n disks sorted in a stack in increasing size from top to bottom.
+        - The goal is to move all disks from peg A to peg C, with the following rules:
+            - Only one disk can be moved at a time.
+            - Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty rod.
+            - No disk may be placed on top of a disk that is smaller than it.
+
+    The solution is found recursively by noticing:
+        - To move k disks from one peg to another, we first need to move (k-1) disks to the spare peg. 
+        - Once the (k-1) disks have been moved to the spare, move disk k to the empty target peg, and proceed to move the remaining (k-1) from the spare to the target.
+        - The trivial base case is for k = 1, where the disk is moved directly to the target.
+*/
+
+#include <iostream>
+#include <list>
+
+class TowerHanoi 
+{
+    private:
+        int move_number;
+        std::list<int> *A, *B, *C; // Pegs are represented by lists
+    
+    public:
+        TowerHanoi(std::list<int> *a, std::list<int> *b, std::list<int> *c) : move_number(0), A(a), B(b), C(c) 
+        {
+            std::cout << "\n";
+            printState();
+        }
+        void rec_solve(int n, std::list<int> *a, std::list<int> *b, std::list<int> *c)
+        {
+            if (n == 1) 
+            {
+                c->push_back(a->back());
+                a->pop_back();
+                move_number++;
+                printState();
+            }
+            else 
+            {
+                rec_solve(n - 1, a, c, b);          
+                c->push_back(a->back());
+                a->pop_back();
+                move_number++;
+                printState();
+                rec_solve(n - 1, b, a, c);
+            }
+        }
+        void printState()
+        {
+            std::cout << "Move number " << move_number << ": ";
+            std::cout << "A = {";
+            for (auto el : (*A))
+            {
+                std::cout << el << ", ";
+            }
+            std::cout << "}, ";
+
+            std::cout << "B = {";
+            for (auto el : (*B))
+            {
+                std::cout << el << ", ";
+            }
+            std::cout << "}, ";
+
+            std::cout << "C = {";
+            for (auto el : (*C))
+            {
+                std::cout << el << ", ";
+            }
+            std::cout << "}\n";
+        }
+
+        friend void solve(TowerHanoi& th);
+};
+
+void solve(TowerHanoi& th)
+{
+    th.rec_solve(th.A->size(), th.A, th.B, th.C);
+}
+
+int main(int argc, char* argv[])
+{
+    int n_disks;
+
+    std::cout << "Tower of Hanoi Solver!\n\n" << "How many disks do you want to solve for? ";
+    std::cin >> n_disks;
+
+    while (n_disks <= 0)
+    {
+        std::cout << "\nPlease provide a positive integer for the number of disks: ";
+        std::cin >> n_disks;
+    }
+
+    std::list<int> a, b, c;
+    
+    for (int i = 1; i <= n_disks; i++)
+    {
+        a.push_front(i);
+    }
+    
+    TowerHanoi tower(&a, &b, &c);
+
+    solve(tower);
+
+    std::cout << "\nThe minimal number of moves is 2^n-1 for n disks!\n";
+
+    return EXIT_SUCCESS;
+}
